b6613b67a607cebeaef26cc59beb040cc60d8165
pankas87 <pankas87@gmail.com>
2013-12-18 23:01:30 -0430

    Create 2013_11_29_191144_create_files_table.php

---
+++ app/database/migrations/2013_11_29_191144_create_files_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class Files extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('files', function($table){
+			$table->increments('id');
+			$table->string('basedir', 255);
+			$table->string('filename', 255);
+			$table->string('title', 255);
+			$table->integer('size')->unsigned();
+			$table->integer('width')->unsigned();
+			$table->integer('height')->unsigned();
+			$table->string('mime_type', 255);
+			$table->timestamps();
+			$table->unique(array('basedir','filename'));
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('files');
+	}
+
+}
